bd74781e338e17333d356b0f9cf07f6c1f2cc431
Jigar Panchal <panchal.jigar90@gmail.com>
2019-01-27 00:36:53 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,15 @@
 
+file_name <- "dataset.zip"
+
+# Download and unzip the given dataset from url :
+if (!file.exists(file_name)){
+  URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file( URL, file_name, method= "curl" )
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(file_name) 
+}
+
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = F)
+features_filter <- grep( "*mean*|*std*", features[ , 2] )
+features_Data <- features[features_filter, 2]
