fd49b5cb9af71f95b762a28dc345fedc15272177
Jigar Panchal <panchal.jigar90@gmail.com>
2019-01-27 03:32:37 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -10,11 +10,6 @@ if (!file.exists("UCI HAR Dataset")) {
   unzip(file_name) 
 }
 
-#
-
-activity_names <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = F, col.names = c("ID", "Label"))
-features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = F)
-
 # Load the datasets
 train_data <- read.table("UCI HAR Dataset/train/X_train.txt", stringsAsFactors = FALSE)
 test_data <- read.table("UCI HAR Dataset/test/X_test.txt", stringsAsFactors = FALSE)
@@ -29,15 +24,31 @@ test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt", stringsAsFac
 all_subject <- rbind(train_subject, test_subject)
 
 #
-
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = F)
 colnames(all_data) <- features[ , 2]
 colnames(all_activity) <- "activity"
 colnames(all_subject) <- "subject"
 
-# cleaning dataframe & grep wanted variable (Mean & Standard Deviation)
-features_filter <- grep( "*mean*|*std*", features[ , 2] )
-features_data <- features[features_filter, 2]
-features_data <- gsub("-mean", " mean", features_data)
-features_data <- gsub("-std", " std", features_data)
-features_data <- gsub("[-()]", "", features_data)
+#
+raw_data <- cbind(all_activity, all_subject, all_data)
 
+# Extracts only the measurements on the mean and standard deviation for each measurement
+extracted_data_index <- grep( "activity|subject|mean\\(\\)|std\\(\\)", colnames(raw_data) )
+extracted_raw_data <- raw_data[, extracted_data_index]
+
+#
+activity_names <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = F, col.names = c("ID", "Label"))
+extracted_raw_data$activity <- factor(extracted_raw_data$activity, levels = activity_names[,1], labels = activity_names[,2])
+
+#
+names(extracted_raw_data)<-gsub("^t", "time", names(extracted_raw_data))
+names(extracted_raw_data)<-gsub("^f", "frequency", names(extracted_raw_data))
+names(extracted_raw_data)<-gsub("Acc", "Accelerometer", names(extracted_raw_data))
+names(extracted_raw_data)<-gsub("Gyro", "Gyroscope", names(extracted_raw_data))
+names(extracted_raw_data)<-gsub("Mag", "Magnitude", names(extracted_raw_data))
+names(extracted_raw_data)<-gsub("BodyBody", "Body", names(extracted_raw_data))
+
+#
+library(dplyr)
+tidy_data <- extracted_raw_data %>% group_by(subject, activity) %>% summarise_each(funs(mean))
+write.table(tidy_data, "tidy_data.txt", row.names = FALSE, quote = FALSE)
